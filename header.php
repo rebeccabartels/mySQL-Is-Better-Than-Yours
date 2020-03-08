f6188ad62e89300847c6f67005c0da9b9b6ae826
rebeccabartels <beccacode@gmail.com>
2020-03-08 19:12:40 -0400

    cleaned up headerfile

--- header.php
+++ header.php
@@ -4,20 +4,13 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<title>Final Project</title>
 </head>
-          <!--<li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>-->
-
         </ul>
         <div class="form-group">
           <input type="text" class="form-control" value="" placeholder="Search">
-        </div>
-      </div>
-    </div>
-  </div>
+</div>
 </header>
 <!-- header end here -->
-<div class="site-wrapper">
+
